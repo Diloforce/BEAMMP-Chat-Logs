0c076b90964f01b0a30f4f4b85c180618c343b68
Diloforce <150070160+Diloforce@users.noreply.github.com>
2024-01-23 16:35:48 +0000

    Update discord_webhook.py

--- discord_webhook.py
+++ discord_webhook.py
@@ -2,7 +2,7 @@ import sys
 import requests
 
 def send_to_discord(message):
-    webhook_url = "https://discord.com/api/webhooks/1199391173102542888/DrAYxf1gq7DGj87vU6mtc50SEcAPzPCL3QXB1U85enIqQdiDKYTgRd8nUwV66pg1UDnR"
+    webhook_url = "YOUR_DISCORD_WEBHOOK_LINK"
     data = {"content": message}
     response = requests.post(webhook_url, json=data)
     print(response.text)
